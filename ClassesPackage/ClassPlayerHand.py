01fdcec1068cb6fa751ebc786146f07245ac0dad
Matheus Camargo <mat.camargo@terra.com.br>
2023-06-14 19:46:30 -0300

    Deleting useless debugs

--- ClassesPackage/ClassPlayerHand.py
+++ ClassesPackage/ClassPlayerHand.py
@@ -1,6 +1,5 @@
 import pyautogui
 from .ClassCoordinates import MatrixCoordinates
-from Helpers.Debug import DebugTxt as Debug
 
 class MaoJogador:
     def __init__(self):
@@ -10,14 +9,10 @@ class MaoJogador:
          self.NumberOfCards = NumberOfCards
 
     def SelectCard(self, PressedKey):
-            Debug("Escolher carta start \n")
             if int(PressedKey) <= self.NumberOfCards and self.NumberOfCards > 0:
-                Debug("Carta valida")
                 CardCoordinate = MatrixCoordinates[self.NumberOfCards - 1][int(PressedKey) - 1]
-                Debug("Coordenadas: X = " + str(CardCoordinate.X) + " Y = " + str(CardCoordinate.Y) + "\n")
                 pyautogui.moveTo(CardCoordinate.X, CardCoordinate.Y)
                 pyautogui.mouseDown()
                 return True
             else:
-                 Debug("EscolherCarta end \n")
                  return False
